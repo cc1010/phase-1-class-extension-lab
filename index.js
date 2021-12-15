e765d97b56cf0391d8579b06c757bb40b26a9f00
Chaim Chazan <chaimyisroel@gmail.com>
2021-12-15 13:29:50 -0500

    commit tree eb9ad6e4ed41b5488b024a9181237d3631a49d62

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+class Polygon {
+	constructor(arr) {
+		this.arr = arr;
+	}
+	get countSides() {
+		return this.arr.length;
+	}
+	get perimeter() {
+		return this.arr.reduce((a, b) => a + b, 0);
+	}
+}
+class Triangle extends Polygon {
+	get isValid() {
+		const sorted = this.arr.sort();
+		return sorted[0] + sorted[1] > sorted[2] ? true : false;
+	}
+}
+class Square extends Polygon {
+	get isValid() {
+		return this.arr.every((element, index, array) => element === array[0]);
+	}
+	get area() {
+		return this.arr[0] * this.arr[0];
+	}
+}
